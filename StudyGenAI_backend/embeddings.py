01ec39bd0a6c353eac3ad0a19f3d90c8c1afd8d4
Sreenivasreddy-2206 <sreenu2206@gmail.com>
2026-08-15 08:37:17 +0530

    Lazy load embedding model

--- StudyGenAI_backend/embeddings.py
+++ StudyGenAI_backend/embeddings.py
@@ -8,15 +8,13 @@ def get_model():
 
     if model is None:
         print("Loading embedding model...")
-        model = SentenceTransformer(
-            "all-MiniLM-L6-v2"
-        )
+        model = SentenceTransformer("all-MiniLM-L6-v2")
 
     return model
 
 
 def create_embeddings(chunks):
 
-    model = get_model()
+    embedding_model = get_model()
 
-    return model.encode(chunks).tolist()
+    return embedding_model.encode(chunks).tolist()
